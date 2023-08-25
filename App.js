5aceb3cc097bb838fe948d9e6bed59c90e7d8ca8
husnainkhan321 <140227998+husnainkhan321@users.noreply.github.com>
2023-08-25 23:46:36 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Muhammad Husnain khan</h1>
+      <h2>191370170</h2>
+    </div>
+  );
+}
